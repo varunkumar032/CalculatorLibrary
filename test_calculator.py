579a0c9b6405cb669b8e3389c070f7dab4572fcd
Varun Kumar G <varunkumar032@gmail.com>
2020-03-09 23:53:16 +0530

    Add multiplication, remove whitespace

--- test_calculator.py
+++ test_calculator.py
@@ -12,6 +12,6 @@ class TestCalculator:
 
     def test_subraction(self):
         assert 2 == calculator.subtract(4, 2)
-        
+
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
